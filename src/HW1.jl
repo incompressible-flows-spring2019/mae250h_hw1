9c32ff86704a4d8dafc7b62dc774065d9380b55b
Jeff Eldredge <jdeldre@g.ucla.edu>
2019-03-27 21:51:51 -0700

    Added a basic trapezoidal quadrature, and associated tests

--- src/HW1.jl
+++ src/HW1.jl
@@ -1,9 +1,26 @@
 module HW1
 
-import Random
-import JSON
+"""
+    numerical_integration(fcn,a,b;N=100) -> Number
 
-greet() = print("Hello to the World!")
-greet_alien() = print("Hello ", Random.randstring(8))
+Numerically integrate a function `fcn` using trapezoidal quadrature. The
+function `fcn` should take a single argument \$x\$. The function is integrated from
+\$x = a\$ to \$x = b\$, and return the result. The optional argument `N` sets the
+number of quadrature steps, and defaults to 100.
+"""
+function numerical_integration(fcn,a::Real,b::Real;N=100)
+
+  dx = (b-a)/N
+  x = a
+  result = 0.5*(fcn(a)+fcn(b))
+  for i = 1:N-1
+    x += dx
+    result += fcn(x)
+  end
+
+  return result*dx
+end
+
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,11 @@
 using Test
+using HW1
 
-@test 1 == 1
+
+fcn(x) = 2*x
+
+@test HW1.numerical_integration(fcn,0,1) ≈ 1.0 atol = 1e-14
+
+fcn(x) = x^3
+
+@test HW1.numerical_integration(fcn,-1,1) ≈ 0.0 atol = 1e-14
