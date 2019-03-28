c09390bcd8f29736b21c8d93137f1cca232b1a04
Jeff Eldredge <jdeldre@g.ucla.edu>
2019-03-28 12:39:29 -0700

    Implemented a circulant form of the tridiagonal solver. Tested. Profiled

--- src/HW1.jl
+++ src/HW1.jl
@@ -1,24 +1,91 @@
 module HW1
 
-"""
-    numerical_integration(fcn,a,b;N=100) -> Number
+# We need FFTW to do the circulant tridiagonal solution
+using FFTW
 
-Numerically integrate a function `fcn` using trapezoidal quadrature. The
-function `fcn` should take a single argument \$x\$. The function is integrated from
-\$x = a\$ to \$x = b\$, and return the result. The optional argument `N` sets the
-number of quadrature steps, and defaults to 100.
+# The next block is the documentation associated with our function. It is
+# available if we type ?HW1.scale_it at a Julia prompt.
 """
-function numerical_integration(fcn,a::Real,b::Real;N=100)
+    scale_it(f::Function,x::Real,c::Real) -> Real
 
-  dx = (b-a)/N
-  x = a
-  result = 0.5*(fcn(a)+fcn(b))
-  for i = 1:N-1
-    x += dx
-    result += fcn(x)
+Takes as input a function `f` and two real-valued arguments `x` and `c` and returns
+value `c*f(x)`.
+
+For example,
+```
+julia> g(x) = x^2
+
+julia> HW1.scale_it(g,1,2)
+2
+```
+"""
+function scale_it(f::Function,x::Real,c::Real)
+  return c*f(x)
+end
+
+"""
+    trisolve(a,b,c,f::Vector{<:Real},sys_type::String) -> Vector{Real}
+
+Solve a tridiagonal system, in which the matrix consists of elements `a`, `b`, and `c`,
+on the subdiagonal, diagonal, and superdiagonal, respectively, and the right-hand
+side of the system is given by vector `f`. The `sys_type` of system can be either `"circulant"`
+or `"regular"`. The solution is returned as a vector the same size as `f`.
+
+For `"circulant"` systems, `a`, `b` and `c` can only be scalars, since the diagonals
+must each be uniform. For `"regular"` systems, these can each be vectors, but `b` must
+be the same length as `f` and `a` and `c` should be one element shorter.
+"""
+function trisolve(a,b,c,f::Vector{<:Real},sys_type::String)
+  # Note that `f` is set up to only accept vectors whose elements are of type Real
+  # or some subtype of Real (e.g. Float64). That is what the <: does.
+
+  M = length(f)
+
+
+  # Return immediately with zeros if f is all zeros
+  if all(f .== 0)
+    return f
   end
 
-  return result*dx
+
+  if sys_type == "circulant"
+    # create a solution vector that is just a copy of f. We will operate on this
+    # vector "in place". We are also converting it to complex type for use in the
+    # fft
+    x = ComplexF64.(f)
+
+    m = 0:M-1
+    # Get the eigenvalues of the matrix
+    # In Julia, you can get special symbols like π by typing backslash, then pi,
+    # then [TAB].
+    lam = b .+ (a+c)*cos.(2π*m/M) .- im*(a-c)*sin.(2π*m/M)
+
+    # x now temporarily holds the transform of the right-hand side
+    fft!(x)
+
+    # Scale x by M to make it agree with what we usually call the discrete
+    # Fourier transform
+    x ./= M
+
+    # now divide the transform of f by the eigenvalues of the matrix
+    # This is a shorthand take each element of x, divide it by the corresponding
+    # entry in lam, and put the result in x.
+    x ./= lam
+
+    # Now inverse transform the result and scale appropriately and return just
+    # the real part of each element
+    ifft!(x)
+
+    x .*= M
+
+    return real.(x)
+
+
+  elseif sys_type == "regular"
+    println("That is your job!")
+    return nothing
+  end
+
 end
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,62 @@
+# A package of useful testing commands
 using Test
+
+# We need to tell this test to load the module we are testing
 using HW1
 
+# The Random package is useful for making tests as arbitrary as possible
+using Random
 
-fcn(x) = 2*x
 
-@test HW1.numerical_integration(fcn,0,1) ≈ 1.0 atol = 1e-14
+#=
+This test checks that scale_it will fail if we try to pass something other than
+a function into the first argument. There should be no form of scale_it that
+works for such a case. We check that it "throws" an error
+=#
+@test_throws MethodError HW1.scale_it(1,1,1)
 
-fcn(x) = x^3
+# This test checks that the desired result is obtained
+fcn(x) = x^2
+@test HW1.scale_it(fcn,1,2) == 2
 
-@test HW1.numerical_integration(fcn,-1,1) ≈ 0.0 atol = 1e-14
+#=
+This check is similar to the previous one, but floating-point tests can
+be challenging because floating-point 0 is never quite zero on a computer.
+Here, we use an approximate check, to within a tolerance that would be
+reasonable for 64-bit floating-point numbers (Float64)
+=#
+fcn(x) = sin(x)
+@test HW1.scale_it(fcn,pi,2) ≈ 0 atol=1e-14
+
+
+# Setting up a separate test set is useful to compartmentalize the testing
+# of different parts of the code
+@testset "trisolve" begin
+
+  # Our routine should not accept arguments that are inappropriate:
+  @test_throws MethodError HW1.trisolve(1,1,1,1,1)
+
+  #=
+  Our routine should return a vector of 0 if the rhs is 0, regardless
+  of the coefficients in the matrix. (Though it would be useful to check
+  for singular matrices
+  =#
+  x = HW1.trisolve(rand(),rand(),rand(),zeros(10),"circulant")
+  @test all(x.== 0)
+
+  #=
+  Now a test of a circulant matrix with non-trivial result. We will test a
+  4x4 system with random choices for coefficients and right-hand side vector
+  and compare our answer with A\rhs
+  =#
+  a = rand(); b = rand(); c = rand()
+
+  A = [b c 0 a; a b c 0; 0 a b c; c 0 a b]
+  rhs = rand(4)
+
+  x = HW1.trisolve(a,b,c,rhs,"circulant") # our solution
+  x2 = A\rhs  # Julia's solution with native solver
+
+  @test x ≈ x2
+
+end
